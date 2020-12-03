c2aa8de2e3fdc3fffec6738eea6639a6f3346b0d
drews <thephantommr@gmail.com>
2020-12-03 19:06:22 +0200

    latest terms check is done

--- controllers/terms.controller.js
+++ controllers/terms.controller.js
@@ -1,4 +1,4 @@
-const { throwError, to, error, success } = require('../utils/requestHelpers');
+const { to, error, success } = require('../utils/requestHelpers');
 const authService = require('../services/auth.service');
 import { user as User } from '../models';
 import { term as Term } from '../models';
@@ -27,23 +27,22 @@ const accept = async (req, res) => {
         return success(res, {message: 'Terms version #'+body.termsVersion+' accepted!'});
     }
 }
-const create = async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    const { body } = req;
-    if (!body.username || !body.email) {
-        return error(res, 'Please enter an email and username to register', 400);
-    } else if (!body.password) {
-        return error(res, 'Please enter an password to register', 400);
-    } else {
-        let err, user;
-        // place for auth service request
-        [err] = await to(authService.register(body));
-        if (err) return error(res, err.message, 400);
-        [err, user] = await to(authService.login(body));
-        console.log(user);
-        return success(res, {message:'Successfully created new user.', user: user}, 201);
-    }
-};
+
+
+const latest = async (req, res) => {
+    let err, user, term;
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    [err, user] = await to(User.findAll({where: { email: authService.verify(token)['user_email'] } }));
+    user = user[0];
+    if (err) return error(res, err.message, 400);
+    if(!user) return error(res, "no user found!", 400);
+    [err, term] = await to(Term.findOne({where: {version: await Term.max('version')} }));
+    if(err) return error(res, err.message, 400);
+    if(user.termsAcceptedVersion === term.version) return success(res);
+        else if (user.termsAcceptedVersion < term.version) return success(res, {content: term.content});
+
+}
 
 exports.accept = accept;
-exports.create = create;
+exports.latest = latest;
--- routes/terms.routes.js
+++ routes/terms.routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 const router = express.Router();
-const UserController = require('../controllers/user.controller');
 const TermsController = require('../controllers/terms.controller');
 const passport = require('passport');
 require('../middleware/passport')(passport);
 
-router.post('/register', UserController.create);
-router.post('/accept', passport.authenticate('jwt', {session:false}), TermsController.accept)
+router.get('/latest', passport.authenticate('jwt', {session:false}), TermsController.latest);
+router.post('/accept', passport.authenticate('jwt', {session:false}), TermsController.accept);
 
 module.exports = router;
