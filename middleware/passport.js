a734cf647b203b0bc62084925044e163c0abc16b
drews <thephantommr@gmail.com>
2020-12-04 00:50:38 +0200

    google auth added

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,13 +1,22 @@
 const { ExtractJwt, Strategy } = require('passport-jwt');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 import { user as User } from '../models';
 const { JWT_ENCRYPTION } = process.env;
 const { to } = require('../utils/requestHelpers');
 
+require('dotenv').config();
+const { GOOGLE_CLIENT_ID } = process.env;
+const { GOOGLE_CLIENT_SECRET } = process.env;
+const { FACEBOOK_CLIENT_ID } = process.env;
+const { FACEBOOK_CLIENT_SECRET } = process.env;
+const { OAUTH_REDIRECT_URL } = process.env;
 
 module.exports = function (passport) {
   let opts = {};
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
   opts.secretOrKey = JWT_ENCRYPTION;
+
   passport.use(new Strategy(opts, async function (jwt_payload, done) {
     let err, user;
     [err, user] = await to(User.findOne({where: {email: jwt_payload.user_email} }));
@@ -18,5 +27,38 @@ module.exports = function (passport) {
       return done(null, false);
     }
   }));
+
+
+  passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: OAUTH_REDIRECT_URL + "/googleRedirect"
+      },
+      function(accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        console.log("GOOGLE BASED OAUTH VALIDATION GETTING CALLED")
+        return done(null, profile)
+      }
+  ))
+
+  passport.use(new FacebookStrategy({
+        clientID: FACEBOOK_CLIENT_ID,
+        clientSecret: FACEBOOK_CLIENT_SECRET,
+        callbackURL: OAUTH_REDIRECT_URL + "/facebookRedirect",
+        profileFields: ['id', 'displayName', 'email', 'picture']
+      },
+      function(accessToken, refreshToken, profile, done) {
+        console.log(profile)
+        console.log("FACEBOOK BASED OAUTH VALIDATION GETTING CALLED")
+        return done(null, profile)
+      }));
+
+    // These functions are required for getting data To/from JSON returned from Providers
+    passport.serializeUser(function(user, done) {
+        done(null, user)
+    })
+    passport.deserializeUser(function(obj, done) {
+        done(null, obj)
+    })
 };
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,8 @@ import express from 'express';
 const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 require('../middleware/passport')(passport);
 
 router.post('/register', UserController.create);
@@ -9,5 +11,12 @@ router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
 router.put('/updateUser', passport.authenticate('jwt', {session:false}), UserController.update)
 router.get('/isUsernameAvalaible', UserController.isAvalaible);
+
+// OAuth Authentication, Just going to this URL will open OAuth screens
+router.get('/login/google',  passport.authenticate('google', { scope: ['profile','email'] }));
+router.get('/login/facebook',  passport.authenticate('facebook', {scope:'email'}));
+// Oauth user data comes to these redirectURLs
+router.get('/googleRedirect', passport.authenticate('google'), UserController.loginGoogle);
+router.get('/facebookRedirect', passport.authenticate('facebook', {scope: 'email'}), UserController.loginFacebook);
 
 module.exports = router;
