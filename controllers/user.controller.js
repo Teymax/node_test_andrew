063fbaaf698b69186fe647a35e1e434de840eece
drews <thephantommr@gmail.com>
2020-12-03 14:37:06 +0200

    userUpdate finalized

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
 const { throwError, to, error, success } = require('../utils/requestHelpers');
 const authService = require('../services/auth.service');
-// import { user as User } from '../models';
+import { user as User } from '../models';
 
 const create = async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
@@ -28,6 +28,42 @@ const login = async (req, res) => {
   return success(res, user);
 };
 
+const update = async (req, res) => {
+  let err, user;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  [err, user] = await to(User.findOne({ email: authService.verify(token) }));
+  if (err) return error(res, err.message, 400);
+  if(!user) return error(res, "no user found!", 400);
+  if (req.body.username){
+    [err, user] = await to(user.update({username: req.body.username}));
+    if(err) return error(res, err.message, 400);
+  }
+  if (req.body.firstName){
+    [err, user] = await to(user.update({firstName: req.body.firstName}));
+    if(err) return error(res, err.message, 400);
+  }
+  if (req.body.lastName){
+    [err, user] = await to(user.update({lastName: req.body.lastName}));
+    if(err) return error(res, err.message, 400);
+  }
+  if (req.body.country){
+    [err, user] = await to(user.update({country: req.body.country}));
+    if(err) return error(res, err.message, 400);
+  }
+  if (req.body.email){
+    [err, user] = await to(user.update({email: req.body.email}));
+    if(err) return error(res, err.message, 400);
+  }
+  if (req.body.new_password && !req.body.old_password) return error(res, "Enter old password to change it", 400);
+  if (req.body.new_password && req.body.old_password) {
+    [err, user] = await to(user.comparePassword(req.body.old_password));
+    if (err) return error(res, err.message, 400);
+    [err, user] = await to(user.update({password: req.body.new_password}));
+    if (err) return error(res, err.message, 400);
+  }
+  return success(res, {user: user});
+};
 
 const logout = async (req, res) => {
   req.logout();
@@ -37,3 +73,4 @@ const logout = async (req, res) => {
 exports.create = create;
 exports.login = login;
 exports.logout = logout;
+exports.update = update;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 const router = express.Router();
 const UserController = require('../controllers/user.controller');
+const passport = require('passport');
+require('../middleware/passport')(passport);
 
 router.post('/register', UserController.create);
 router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
+router.put('/update', passport.authenticate('jwt', {session:false}), UserController.update)
+
 module.exports = router;
